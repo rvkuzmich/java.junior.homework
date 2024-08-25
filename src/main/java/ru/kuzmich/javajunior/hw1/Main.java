42bc4eabf9533d2b4489ea14cdbbc9e575bb4c5f
Roman Kuzmich <rvku2mich@yandex.ru>
2024-08-25 22:47:20 +0300

    homework1 completed

---
+++ src/main/java/ru/kuzmich/javajunior/hw1/Main.java
@@ -0,0 +1,38 @@
+package ru.kuzmich.javajunior.hw1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/*Напишите программу, которая использует Stream API для обработки списка чисел. Программа должна вывести
+на экран среднее значение всех четных чисел в списке.
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> integers = fillArray();
+        System.out.printf("Начальный список - %s", integers);
+        System.out.println();
+        AtomicInteger sum = new AtomicInteger();
+        integers = integers.stream().filter(num -> num % 2 == 0 && num > 0).toList();
+        if (integers.isEmpty()) {
+            System.out.println("Четных чисел в списке нет");
+        } else {
+            System.out.printf("Отфильтрованный список - %s", integers);
+            integers.forEach(sum::addAndGet);
+            System.out.println();
+            double n = sum.doubleValue();
+            System.out.printf("Среднее значение четных чисел в списке - %.3f", n / integers.size());
+        }
+
+    }
+    public static ArrayList<Integer> fillArray() {
+        Random random = new Random();
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            integers.add(random.nextInt(10));
+        }
+        return integers;
+    }
+}
